31d1125fd8d9b221a200420c489babd9b29f4352
Joel Tio <joeltiojinhon@gmail.com>
2020-11-02 15:04:21 +0800

    refactor: replace static_assert with concepts require

--- sim/src/util/typeMap.h
+++ sim/src/util/typeMap.h
@@ -11,22 +11,16 @@ namespace util {
     private:
         std::unordered_map<std::type_index, V> map;
     public:
-        template<typename T> T at() {
-            static_assert(
-                std::is_base_of<V, T>::value,
-                "Attempting to retrieve non-derived type from type map"
-            );
-
+        template<typename T>
+        requires std::derived_from<T, V>
+        T at() {
             auto type = std::type_index(typeid(T));
             return static_cast<T>(map.at(type));
         };
 
-        template<typename T> void insert(T value) {
-            static_assert(
-                std::is_base_of<V, T>::value,
-                "Attempting to insert non-derived type into type map"
-            );
-
+        template<typename T>
+        requires std::derived_from<T, V>
+        void insert(T value) {
             auto type = std::type_index(typeid(T));
             map.insert(std::make_pair(type, value));
         }
