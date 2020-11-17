6a6820b3cc2db84490854bfb30d0f66bd844b24d
Zhu Zhanyan <program.nom@gmail.com>
2020-11-17 16:46:40 +0800

    build: Add pbr to set reasonable defaults for setuptools

--- sdk/setup.py
+++ sdk/setup.py
@@ -5,7 +5,7 @@
 #
 
 import os
-import setuptools
+from setuptools import setup
 from pathlib import Path
 
 
@@ -13,20 +13,10 @@ parent_dir = Path(os.getcwd()).parent
 with open(os.path.join(parent_dir, "README.md"), "r") as f:
     long_description = f.read()
 
-setuptools.setup(
-    name="bento-box",  # Replace with your own username
-    version="0.0.1-dev",
-    author="The Bentobox Authors",
-    author_email="mrzzy.dev@gmail.com",
-    description="SDK for Bentobox",
+setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/joeltio/bento-box",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
+    setup_requires=["pbr>=5"],
+    pbr=True,
 )
