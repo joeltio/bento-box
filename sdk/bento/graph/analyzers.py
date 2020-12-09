3416a8451fa449db21d58f90914c515ea40c4aa5
Zhu Zhanyan <program.nom@gmail.com>
2020-12-10 00:18:56 +0800

    feat(sdk): Add analyze_const and analyze_assign analyzers to analyze constants and assignments

--- sdk/bento/graph/analyzers.py
+++ sdk/bento/graph/analyzers.py
@@ -8,13 +8,27 @@ import gast
 
 from math import inf
 from typing import List
-from gast import AST, FunctionDef, ClassDef, Lambda, Pass, Expr, Constant
+from gast import (
+    AST,
+    Constant,
+    List,
+    Tuple,
+    Pass,
+    FunctionDef,
+    Expr,
+    Load,
+    Del,
+    Store,
+    Assign,
+    AugAssign,
+    AnnAssign,
+)
 
 
 def analyze_func(ast: AST) -> AST:
     """Annotate `FunctionDef` nodes in the given AST with additional infomation
 
-    Walks through the `FunctionDef` nodes in given AST and insects annotates
+    Walks through the `FunctionDef` nodes in given AST and annotates
     each node with the following info:
     - `n_args`: the function's arguments count.
     - `docstr`: the function's docstring if present, otherwise None
@@ -55,7 +69,7 @@ def analyze_func(ast: AST) -> AST:
 
 
 def analyze_convert_fn(ast: AST) -> AST:
-    """Finds and Annotates the target convert function
+    """Finds and Annotates the target convert function AST
 
     Assumes the target convert function is the first child node of AST.
     Annotates the top level node with the target convert `FunctionDef` node as
@@ -66,9 +80,118 @@ def analyze_convert_fn(ast: AST) -> AST:
             AST to scan and annotate the target convert function.
 
     Returns:
-        The given AST with with the target convert function annotated as `convert_fn`
+        The given AST with the target convert function annotated as `convert_fn`
     """
     # walk through the AST to find the top level node with min nesting
     canidates_fn = [n for n in gast.iter_child_nodes(ast) if isinstance(n, FunctionDef)]
     ast.convert_fn = canidates_fn[0] if len(canidates_fn) == 1 else None
+    return ast
+
+
+def analyze_assign(ast: AST) -> AST:
+    """Finds and Annotes assignments in the given AST with additional infomation
+
+    Walks through the assignment nodes in given AST and annotates
+    each node with the following info:
+    - `n_targets`: no. of targets variables this assignments assign to.
+    - `is_unpack`: Whether this assignment unpacks values from a List or Tuple.
+    - `is_multi`: Whether this assignment assigns the same value to multiple variables.
+    - `n_values`: no. of values this assignment attempts to assign.
+    Annotates assignment's targets and values with reference to assignment `assign`.
+
+    Args:
+        ast:
+            AST to scan and label constant literals.
+    Returns:
+        The given AST with the assignments annotated.
+    """
+    assign_types = (
+        Assign,
+        AnnAssign,
+        AugAssign,
+    )
+    assign_asts = [n for n in gast.walk(ast) if isinstance(n, assign_types)]
+
+    for assign_ast in assign_asts:
+        iterable_types = (List, Tuple)
+        assign_ast.is_unpack, assign_ast.is_multi = False, False
+        if isinstance(assign_ast, Assign):
+            # check if this unpacking assignment
+            targets = assign_ast.targets
+            if isinstance(assign_ast.targets[0], iterable_types):
+                targets = assign_ast.targets[0].elts
+                assign_ast.is_unpack = True
+            # check if this multiple assignment
+            elif len(assign_ast.targets) > 1:
+                assign_ast.is_multi = True
+        else:
+            targets = [assign_ast.target]
+        # determine no. of target variables assigned
+        assign_ast.n_targets = len(targets)
+
+        # determine no. of values assigned
+        if isinstance(assign_ast.value, iterable_types):
+            values = assign_ast.value.elts
+            assign_ast.n_values = len(values)
+        else:
+            values = [assign_ast.value]
+        assign_ast.n_values = len(values)
+
+        # create back references in assignment targets and values
+        if isinstance(assign_ast, Assign) and isinstance(
+            assign_ast.targets[0], iterable_types
+        ):
+            assign_ast.targets[0].assign = assign_ast
+        assign_ast.value.assign = assign_ast
+        for child_node in targets + values:
+            child_node.assign = assign_ast
+
+    return ast
+
+
+def analyze_const(ast: AST) -> AST:
+    """Finds and labels constant literals in the given AST.
+
+    Detects constant literals with the criteria:
+    - Nodes of the Constant AST type.
+    - List/Tuple ASTs containing or Constant nodes. Nested List/Tuples are
+        allowed they contain Constant nodes. Note that the entire List/Tuple
+        would be recognised as one Constant unless it is un unpacking expression.
+    and annotates the constant literals with `is_constant` to `True`
+
+    Args:
+        ast:
+            AST to scan and label constant literals.
+    Returns:
+        The given AST with the constants literals annotated with `is_constant`
+    """
+
+    # recursively walk AST to search for constants
+    def walk_const(ast, part_of=None) -> AST:
+        iterable_types = (List, Tuple)
+        ignore_types = (Load, Store, Del)
+
+        ast.is_constant = False
+        if isinstance(ast, ignore_types):
+            pass
+        # part of collection but not constant, meaning collection is not constant
+        elif part_of is not None and not isinstance(ast, iterable_types + (Constant,)):
+            part_of.is_constant = False
+        # constant if constant type and and not part of any larger collection
+        elif part_of is None and isinstance(ast, Constant):
+            ast.is_constant = True
+        # make sure we are not unpacking from the iterable
+        # otherwise elements should be recognised as constants instead
+        elif isinstance(ast, iterable_types) and not (
+            hasattr(ast, "assign") and ast.assign.is_unpack
+        ):
+            # mark child nodes as part of this collection node
+            part_of = ast
+            # mark this collection node as constant unless a child node overrides
+            ast.is_constant = True
+        # recursively inspect child nodes for constants
+        for node in gast.iter_child_nodes(ast):
+            walk_const(node, part_of)
+
+    walk_const(ast)
     return ast
--- sdk/tests/graph/test_analyzers.py
+++ sdk/tests/graph/test_analyzers.py
@@ -78,11 +78,90 @@ def test_convert_fn_analyzer():
         (NotConvertFn, False),
         (convert_fn, True),
     ]
+    analyzed_asts = [
+        analyze_convert_fn(analyze_func(parse_ast(f[0]))) for f in convert_fns
+    ]
 
-    analyzed_asts = [analyze_convert_fn(parse_ast(f[0])) for f in convert_fns]
-    assert all(
-        [
-            (ast.convert_fn is not None) == expected_fn[1]
-            for ast, expected_fn in zip(analyzed_asts, convert_fns)
-        ]
+    def check_ast(fn_ast, expected_fn):
+        assert (fn_ast is not None) == expected_fn[1]
+        if fn_ast is not None:
+            assert fn_ast.plotter_name == fn_ast.args.args[0].id
+
+    for ast, expected_fn in zip(analyzed_asts, convert_fns):
+        check_ast(ast.convert_fn, expected_fn)
+
+
+# test assign analyzer annotates assignments
+def test_assign_analyzer():
+    def single_assign():
+        x = 2
+
+    def multi_assign():
+        a = b = True
+
+    def unpack_assign():
+        x, y = 1, 2
+
+    assign_fns = [
+        (
+            single_assign,
+            {"n_targets": 1, "n_values": 1, "is_unpack": False, "is_multi": False},
+        ),
+        (
+            multi_assign,
+            {"n_targets": 2, "n_values": 1, "is_unpack": False, "is_multi": True},
+        ),
+        (
+            unpack_assign,
+            {"n_targets": 2, "n_values": 2, "is_unpack": True, "is_multi": False},
+        ),
+    ]
+
+    assign_types = (
+        Assign,
+        AnnAssign,
+        AugAssign,
     )
+    analyzed_asts = [analyze_assign(parse_ast(f[0])) for f in assign_fns]
+    for ast, assign_fn in zip(analyzed_asts, assign_fns):
+        assign_ast = [n for n in gast.walk(ast) if isinstance(n, assign_types)][0]
+
+        # check ast has expected annotations
+        _, expected_annotations = assign_fn
+        assert all(
+            [
+                getattr(assign_ast, name) == value
+                for name, value in expected_annotations.items()
+            ]
+        )
+
+        # TODO(mrzzy): check backreferences are set correctly
+
+
+# test that constants picked up & labeled by constants analyzer
+def test_const_analyzer():
+    def convert_fn(g):
+        int_const = 2
+        float_const = 3.14
+        str_const = "str"
+        list_const = [1, 2, 3]
+        tuple_const = (1, 2, 3)
+        boolean_const, boolean_const_2 = True, False
+
+    req_analyzers = [
+        analyze_func,
+        analyze_convert_fn,
+        analyze_assign,
+    ]
+    ast = parse_ast(convert_fn)
+    for analyzer in req_analyzers:
+        ast = analyzer(ast)
+    analyzed_ast = analyze_const(ast)
+    fn_ast = analyzed_ast.convert_fn
+    get_const_ast = lambda i: fn_ast.body[i].value
+    expected_asts = set(
+        [get_const_ast(i) for i in range(5)]
+        + [const for const in fn_ast.body[5].value.elts]
+    )
+    const_asts = set([n for n in gast.walk(ast) if n.is_constant])
+    assert const_asts == expected_asts
