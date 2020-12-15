198b32c7136f7bd0488361ef1b27f010e5cfa0de
Joel Tio <joeltiojinhon@gmail.com>
2020-12-15 16:26:04 +0800

    feat: add indexStore

---
+++ sim/lib/core/include/core/ics/indexStore.h
@@ -0,0 +1,10 @@
+#ifndef BENTOBOX_INDEXSTORE_H
+#define BENTOBOX_INDEXSTORE_H
+
+#include "util/typeMap.h"
+
+namespace ics {
+    typedef util::TypeMap IndexStore;
+}
+
+#endif //BENTOBOX_INDEXSTORE_H
