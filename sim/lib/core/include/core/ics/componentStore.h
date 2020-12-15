c0103fa180691e4c315b95fbccebe9f922c6c0e6
Joel Tio <joeltiojinhon@gmail.com>
2020-12-15 14:41:46 +0800

    feat: add basic operations on component store

--- sim/lib/core/include/core/ics/componentStore.h
+++ sim/lib/core/include/core/ics/componentStore.h
@@ -4,10 +4,43 @@
 #include <unordered_map>
 #include <memory>
 #include <any>
+#include "component.h"
+#include "compVec.h"
 
 namespace ics {
-    // size_t -> ptr -> CompVec<Component>
+    // stores size_t -> unique_ptr -> CompVec<Component>
+    // stored as size_t -> unique_ptr -> any(CompVec<BaseComponent>)
     typedef std::unordered_map<size_t, std::unique_ptr<std::any>> ComponentStore;
+
+    template<Component C>
+    CompVec<C>& getCompVec(ComponentStore& store, size_t group) {
+        std::any& compVecAny = *store.at(group);
+        auto& compVec = std::any_cast<ics::CompVec<BaseComponent>&>(compVecAny);
+        auto& castedCompVec = *reinterpret_cast<ics::CompVec<C>*>(&compVec);
+        return castedCompVec;
+    }
+
+    template<Component C>
+    CompVec<C>& createCompVec(ComponentStore& store, size_t group) {
+        ics::CompVec<C> vec;
+        ics::CompVec<BaseComponent> castedVec = *reinterpret_cast<ics::CompVec<BaseComponent>*>(&vec);
+
+        store.emplace(
+            group,
+            std::make_unique<std::any>(
+                std::make_any<ics::CompVec<BaseComponent>>(castedVec)
+            )
+        );
+
+        return getCompVec<C>(store, group);
+    }
+
+    template<Component C>
+    std::pair<size_t, size_t> addComponent(ComponentStore& store, const C& c, size_t group) {
+        auto& vec = store.contains(group) ? getCompVec<C>(store, group) : createCompVec<C>(store, group);
+        auto vecIndex = vec.add(c);
+        return std::make_pair(group, vecIndex);
+    }
 }
 
 #endif //BENTOBOX_COMPONENTSTORE_H
---
+++ sim/lib/core/src/ics/componentStore.test.cpp
@@ -0,0 +1,71 @@
+#include <gtest/gtest.h>
+#include <core/ics/component.h>
+#include <core/ics/compVec.h>
+#include <core/ics/componentStore.h>
+
+#define TEST_SUITE ComponentStoreTest
+
+using namespace ics;
+
+struct TestComponent : public BaseComponent {
+    int height;
+};
+
+TEST(TEST_SUITE, CreateVec) {
+    ComponentStore store;
+
+    ASSERT_EQ(store.size(), 0);
+    createCompVec<TestComponent>(store, 1);
+    ASSERT_EQ(store.size(), 1);
+}
+
+TEST(TEST_SUITE, CreateCompVecReturnsReference) {
+    ComponentStore store;
+    auto& vec = createCompVec<TestComponent>(store, 1);
+    vec.add(TestComponent { true, 20 });
+
+    auto retrievedVec = std::any_cast<CompVec<BaseComponent>>(*store.at(1));
+    ASSERT_EQ(retrievedVec.size(), 1);
+}
+
+TEST(TEST_SUITE, GetComponentsRetrievesComponents) {
+    ComponentStore store;
+    auto& vec = createCompVec<TestComponent>(store, 1);
+    auto compId = vec.add(TestComponent { true, 20 });
+
+    auto retrievedVec = getCompVec<TestComponent>(store, 1);
+    ASSERT_EQ(retrievedVec.at(compId).height, 20);
+}
+
+TEST(TEST_SUITE, GetComponentsRetrievesComponentsByReference) {
+    ComponentStore store;
+    createCompVec<TestComponent>(store, 1);
+
+    auto& vec1 = getCompVec<TestComponent>(store, 1);
+    vec1.add(TestComponent { true, 20 });
+    auto& vec2 = getCompVec<TestComponent>(store, 1);
+    ASSERT_EQ(vec2.size(), 1);
+}
+
+TEST(TEST_SUITE, AddComponentsCreatesNewVec) {
+    ComponentStore store;
+    ASSERT_EQ(store.size(), 0);
+    addComponent(store, TestComponent { true, 20 }, 1);
+    ASSERT_EQ(store.size(), 1);
+}
+
+TEST(TEST_SUITE, AddComponentsAppendsToVec) {
+    ComponentStore store;
+    createCompVec<TestComponent>(store, 1);
+    ASSERT_EQ(store.size(), 1);
+    ASSERT_EQ(getCompVec<TestComponent>(store, 1).size(), 0);
+
+    // Ensure that no new vectors are created
+    auto compFullId = addComponent(store, TestComponent { true, 20 }, 1);
+    ASSERT_EQ(store.size(), 1);
+
+    // Ensure that the item is actually appended
+    auto& vec = getCompVec<TestComponent>(store, 1);
+    ASSERT_EQ(vec.size(), 1);
+    ASSERT_EQ(vec.at(compFullId.second).height, 20);
+}
