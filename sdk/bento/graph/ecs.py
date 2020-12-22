ddb41a07d712793c25248d73cc695ac1b8f7f513
Zhu Zhanyan <program.nom@gmail.com>
2020-12-22 11:29:43 +0800

    feat(sdk): Add GraphNode to wrap Node proto to provide operator overloads during graph construction

--- sdk/bento/graph/ecs.py
+++ sdk/bento/graph/ecs.py
@@ -1,6 +1,11 @@
+#
+# Bentobox
+# SDK - Graph
+# Graph ECS
+#
+
 from random import randint
-from typing import Any, Iterable, Set
-from bento.value import wrap
+from typing import Any, Iterable, Set, List
 from bento.ecs.base import Component, Entity
 from bento.graph.value import wrap_const
 from bento.protos.graph_pb2 import Node
@@ -8,11 +13,165 @@ from bento.protos.references_pb2 import AttributeRef
 from bento.protos.values_pb2 import Value
 
 
+class GraphNode:
+    """Shim that wraps a Node proto to provide operations when building a graph.
+
+    Provides operator overloads for Node proto to provide operations during
+    computational graph constructions. Records the operations performed on the Node proto.
+
+
+    The wrapped Node proto can be obtained via `.node`.
+    """
+
+    def __init__(self, node: Node):
+        self.node = node
+
+    @classmethod
+    def from_attr(cls, entity_id: int, component: str, name: str):
+        """Create a GraphNode from the specified ECS attribute"""
+        return GraphNode.wrap(
+            Node.Retrieve(
+                retrieve_attr=AttributeRef(
+                    entity_id=entity_id,
+                    component=component,
+                    attribute=name,
+                )
+            )
+        )
+
+    @classmethod
+    def wrap(cls, val: Any):
+        """Wraps the given value val as a Graph, performing type conversions if necessary.
+        Args:
+            val: Native Value, Node proto to wrap as GraphNode
+        Returns:
+            The given value wrapped as GraphNode
+        """
+        # check if we are already GraphNode, if so no conversions required.
+        if isinstance(val, cls):
+            return val
+        # convert to Constant node if native value
+        node = val if isinstance(val, Node) else wrap_const(val)
+        return GraphNode(node=node)
+
+    # arithmetic overloads
+    def __add__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(add_op=Node.Add(x=self.node, y=other.node)))
+
+    def __radd__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(add_op=Node.Add(x=other.node, y=self.node)))
+
+    def __iadd__(self, other: Any):
+        other = GraphNode.wrap(other)
+        # add inplace
+        self.node = Node(add_op=Node.Add(x=self.node, y=other.node))
+        return self
+
+    def __sub__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(sub_op=Node.Sub(x=self.node, y=other.node)))
+
+    def __rsub__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(sub_op=Node.Sub(x=other.node, y=self.node)))
+
+    def __isub__(self, other: Any):
+        other = GraphNode.wrap(other)
+        # subtract inplace
+        self.node = Node(add_op=Node.Sub(x=self.node, y=other.node))
+        return self
+
+
+    def __mul__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(mul_op=Node.Mul(x=self.node, y=other.node)))
+
+    def __rmul__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(mul_op=Node.Mul(x=other.node, y=self.node)))
+
+    def __imul__(self, other: Any):
+        other = GraphNode.wrap(other)
+        # multiply inplace
+        self.node = Node(add_op=Node.Mul(x=self.node, y=other.node))
+        return self
+
+
+    def __truediv__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(div_op=Node.Div(x=self.node, y=other.node)))
+
+    def __truediv__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(div_op=Node.Div(x=other.node, y=self.node)))
+
+    def __itruediv__(self, other: Any):
+        other = GraphNode.wrap(other)
+        # divide inplace
+        self.node = Node(add_op=Node.Div(x=self.node, y=other.node))
+        return self
+
+
+    def __mod__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(mod_op=Node.Mod(x=self.node, y=other.node)))
+
+    def __rmod__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(mod_op=Node.Mod(x=other.node, y=self.node)))
+
+    def __neg__(self):
+        other = GraphNode.wrap(other)
+        # -x is equvilent to 0 - x
+        return GraphNode.wrap(Node(sub_op=Node.Sub(x=wrap_const(0), y=self.node)))
+
+    def __pos__(self):
+        # +x is equvilent to x
+        return GraphNode.wrap(self.node)
+
+    # boolean overloads
+    def __lt__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(lt_op=Node.Lt(x=self.node, y=other.node)))
+
+    def __gt__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return other.__lt__(self)
+
+    def __eq__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(Node(eq_op=Node.Eq(x=self.node, y=other.node)))
+
+    def __ne__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return Node(not_op=Node.Not( x=self.__eq__(other).node))
+
+    def __le__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(
+            Node(
+                or_op=Node.Or(x=self.__lt__(other.node).node, y=self.__eq__(other.node).node),
+            )
+        )
+
+    def __ge__(self, other: Any):
+        other = GraphNode.wrap(other)
+        return GraphNode.wrap(
+            Node(
+                or_op=Node.Or(x=self.__gt__(other.node).node, y=self.__eq__(other.node).node),
+            )
+        )
+
+
 class GraphComponent(Component):
     """Shim that reprsents ECS Component when plotting computation graph.
 
     Provides access to component's attributes during graph plotting.
-    Records attribute get/sets performed on component graph input/output nodes
+    Records attribute gey(components
+    ]y(components
+    ])fa
     which can be accessed via `.inputs` and `.outputs` respectively.
 
     Example:
@@ -33,38 +192,44 @@ class GraphComponent(Component):
 
     def get_attr(self, name: str) -> Node:
         # Record the attribute retrieve operation as input graph node
-        get_op = Node.Retrieve(
-            retrieve_attr=AttributeRef(
-                entity_id=self._entity_id,
-                component=self._name,
-                attribute=name,
+        get_op = GraphNode(
+            node=Node(
+                retrieve_op=Node.Retrieve(
+                    retrieve_attr=AttributeRef(
+                        entity_id=self._entity_id,
+                        component=self._name,
+                        attribute=name,
+                    )
+                )
             )
         )
         self._inputs.append(get_op)
-
-        return Node(retrieve_op=get_op)
+        return get_op
 
     def set_attr(self, name: str, value: Any):
-        # wrap if native value as a constant node, leave nodes untouched
-        value = value if isinstance(value, Node) else wrap_const(value)
+        value = GraphNode.wrap(value)
         # Record the attribute set/mutate operation as output graph node
-        set_op = Node.Mutate(
-            mutate_attr=AttributeRef(
-                entity_id=self._entity_id,
-                component=self._name,
-                attribute=name,
-            ),
-            to_node=value,
+        set_op = GraphNode(
+            node=Node(
+                mutate_op=Node.Mutate(
+                    mutate_attr=AttributeRef(
+                        entity_id=self._entity_id,
+                        component=self._name,
+                        attribute=name,
+                    ),
+                    to_node=value.node,
+                )
+            )
         )
         self._outputs.append(set_op)
 
     @property
-    def inputs(self):
+    def inputs(self) -> List[GraphNode]:
         """Get the graph input nodes recorded by this Graph component"""
         return self._inputs
 
     @property
-    def outputs(self):
+    def outputs(self) -> List[GraphNode]:
         """Get the graph output nodes recorded by this Graph component"""
         return self._outputs
 
@@ -80,25 +245,25 @@ class GraphEntity(Entity):
     The GraphEntity's GraphComponents can be accessed via `.components`.
     """
 
-    def __init__(self, components: Set[str]):
+    def __init__(self, components: Iterable[str]):
         # TODO(zzy): obtain id from actual entity
         self.id = randint(1, int(1e5))
         self.component_map = {
             name: GraphComponent(self.id, name) for name in components
         }
 
-    def get_component(self, name):
+    def get_component(self, name: str) -> GraphComponent:
         try:
             return self.component_map[name]
         except KeyError:
             raise ValueError(
-                f"Cannot get component: Component {name} not found bound for entity"
+                f"Cannot get component: Component {name} not attached for entity"
             )
 
     @property
-    def components(self):
+    def components(self) -> Set[str]:
         """Get the GraphComponents attached to this GraphEntity"""
-        return self.component_map.values()
+        return set(self.component_map.values())
 
     def __str__(self):
         # return string representation of graph entity
---
+++ sdk/tests/graph/test_ecs.py
@@ -0,0 +1,79 @@
+#
+# Bentobox
+# SDK - Graph
+# Graph ECS
+#
+
+from bento.graph.ecs import *
+
+
+def test_graph_ecs_entity():
+    components = ["position"]
+    entity = GraphEntity(components=components)
+    # check Entity's components accessible via `.components`
+    assert len(set([c._name for c in entity.components]) - set(components)) == 0
+    # check component accessible by name using [] notation
+    position = entity[components[0]]
+    assert isinstance(position, GraphComponent)
+
+
+def test_graph_ecs_component_get_attr():
+    entity_id, name = 1, "position"
+    position = GraphComponent(entity_id=entity_id, name=name)
+    # check that getting an attribute from a component returns a GraphNode
+    # wrapping a Retrieve node that retrieves the attribute
+    pos_x = position.x
+    expected_node = Node(
+        retrieve_op=Node.Retrieve(
+            retrieve_attr=AttributeRef(entity_id=entity_id, component=name, attribute="x")
+        )
+    )
+    assert pos_x.node == expected_node
+    # check tha component records the retrieve in `.inputs`
+    assert position.inputs[0].node == expected_node
+
+
+def test_graph_ecs_component_set_attr_node():
+    entity_id, name = 1, "position"
+    position = GraphComponent(entity_id=entity_id, name=name)
+    pos_x = position.x
+    # check setting attribute to node sets expected output node node.
+    position.y = pos_x
+    expected_node = Node(
+        mutate_op=Node.Mutate(
+            mutate_attr=AttributeRef(
+                entity_id=entity_id,
+                component="position",
+                attribute="y",
+            ),
+            to_node=pos_x.node,
+        )
+    )
+    assert position.outputs[0].node == expected_node
+
+def test_graph_ecs_component_set_attr_native_value():
+    entity_id, name = 1, "position"
+    position = GraphComponent(entity_id=entity_id, name=name)
+    # check setting attribute to native sets expected output node node.
+    position.y = 3.1
+    expected_node = Node(
+        mutate_op=Node.Mutate(
+            mutate_attr=AttributeRef(
+                entity_id=entity_id,
+                component="position",
+                attribute="y",
+            ),
+            to_node=wrap_const(3.1)
+        )
+    )
+    assert position.outputs[0].node == expected_node
+
+def test_graph_ecs_node_wrap():
+    wrap_cases = [
+        [ "w", GraphNode(node=wrap_const("w")) ],
+        [ wrap_const(1), GraphNode(node=wrap_const(1)) ],
+        [ GraphNode(node=wrap_const(True)), GraphNode(node=wrap_const(True)) ],
+    ]
+
+    for val, expected in wrap_cases:
+        assert GraphNode.wrap(val) == expected
