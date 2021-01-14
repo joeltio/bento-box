8dccd077b727886350289ab0865056b3773c6379
Zhu Zhanyan <program.nom@gmail.com>
2021-01-15 01:25:15 +0800

    chore: Add test for EngineServiceImpl::GetVersion

--- sim/include/service/engineService.h
+++ sim/include/service/engineService.h
@@ -5,11 +5,11 @@
  * Engine gRPC Service
  */
 
-#include <bento/protos/services.grpc.pb.h>
+#include "bento/protos/services.grpc.pb.h"
 
 class EngineServiceImpl final : public bento::protos::EngineService::Service {
    public:
-    /** Get version information about the engine */
+    // See services.proto for documentation on service calls
     grpc::Status GetVersion(grpc::ServerContext *context,
                             const bento::protos::GetVersionReq *request,
                             bento::protos::GetVersionResp *response) override;
---
+++ sim/src/service/engineService.test.cpp
@@ -0,0 +1,62 @@
+/*
+ * bentobox-sim
+ * Engine gRPC Service tests
+ */
+
+#include <gtest/gtest.h>
+#include <grpc++/grpc++.h>
+
+#include "git.h"
+#include "network/grpcServer.h"
+#include "service/engineService.h"
+
+#define TEST_SUITE EngineServiceImpl
+#define TEST_PORT 50011
+
+using bento::protos::EngineService;
+using bento::protos::GetVersionReq;
+using bento::protos::GetVersionResp;
+using grpc::ClientContext;
+using grpc::CreateChannel;
+using grpc::InsecureChannelCredentials;
+using grpc::Service;
+using grpc::Status;
+using network::GRPCServer;
+using std::list;
+using std::make_shared;
+using std::make_unique;
+using std::move;
+using std::pair;
+using std::string;
+using std::to_string;
+using std::unique_ptr;
+
+// creates test fixtures for engine service tests
+class EngineServiceTest : public testing::Test {
+   protected:
+    // test gRPC server running engine service
+    unique_ptr<GRPCServer> server;
+    unique_ptr<EngineService::Stub> client;
+    unique_ptr<EngineServiceImpl> engineSvc;
+
+    EngineServiceTest() {
+        // setup test server serving engine
+        engineSvc = make_unique<EngineServiceImpl>();
+        list<Service *> services = {engineSvc.get()};
+        server = make_unique<GRPCServer>("localhost", TEST_PORT, services);
+        string address = "localhost:" + to_string(TEST_PORT);
+
+        // setup test client that talks to the test server
+        auto channel = CreateChannel(address, InsecureChannelCredentials());
+        client = move(EngineService::NewStub(channel));
+    }
+};
+
+TEST_F(EngineServiceTest, GetVersion) {
+    GetVersionReq versionReq;
+    GetVersionResp versionResp;
+    ClientContext context;
+    Status s = client->GetVersion(&context, versionReq, &versionResp);
+    ASSERT_TRUE(s.ok());
+    ASSERT_EQ(versionResp.commit_hash(), GIT_HASH);
+}
