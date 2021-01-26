a9606b8e46fba0b14f7683c200ca9eb645f420bb
Joel Tio <joeltiojinhon@gmail.com>
2021-01-26 15:27:34 +0800

    feat: update ics.h to use new component type index

--- sim/include/ics.h
+++ sim/include/ics.h
@@ -1,19 +1,21 @@
 #ifndef BENTOBOX_COMPONENTSTOREEXT_H
 #define BENTOBOX_COMPONENTSTOREEXT_H
 
+#include <component/userComponent.h>
 #include <core/ics/component.h>
 #include <core/ics/componentStore.h>
 #include <index/componentTypeIndex.h>
 #include <index/indexStore.h>
 
 namespace ics {
-template <Component C>
+template <class C>
+requires std::is_base_of_v<component::UserComponent, C>
 CompStoreId addComponent(index::IndexStore& indexStore,
                          ComponentStore& compStore, const C& c) {
     auto& compTypeIndex = indexStore.componentType;
 
     // Update the ComponentType index
-    auto compIndex = compTypeIndex.addComponentType<C>();
+    auto compIndex = compTypeIndex.addComponentType(c.typeName);
     // Add the component to the component store
     auto compId = addComponent(compStore, c, compIndex);
 
