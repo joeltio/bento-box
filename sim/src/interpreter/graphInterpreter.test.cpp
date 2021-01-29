654a9ac9f3652f3b0efe7b6fc9f3f7a01aea19f3
Joel Tio <joeltiojinhon@gmail.com>
2021-01-29 16:47:15 +0800

    feat: add test for switch op

--- sim/src/interpreter/graphInterpreter.test.cpp
+++ sim/src/interpreter/graphInterpreter.test.cpp
@@ -27,7 +27,8 @@ bento::protos::ComponentDef createCompDef() {
 
 struct TestComponent : public ics::component::UserComponent {
     TestComponent() : UserComponent(TEST_COMPONENT_NAME, createCompDef()) {}
-    TestComponent(int width, int height) : UserComponent(TEST_COMPONENT_NAME, createCompDef()) {
+    TestComponent(int width, int height)
+        : UserComponent(TEST_COMPONENT_NAME, createCompDef()) {
         auto widthVal = bento::protos::Value();
         widthVal.mutable_primitive()->set_int_64(width);
         setValue("width", widthVal);
@@ -40,19 +41,22 @@ struct TestComponent : public ics::component::UserComponent {
 
 }  // namespace
 
-class StoresFixture : public::testing::Test {
+class StoresFixture : public ::testing::Test {
    protected:
     ics::ComponentStore compStore;
     ics::index::IndexStore indexStore;
     // First entity
-    ics::index::EntityIndex::EntityId entity1Id = indexStore.entity.addEntityId();
+    ics::index::EntityIndex::EntityId entity1Id =
+        indexStore.entity.addEntityId();
     TestComponent comp1{50, 30};
 
     // Second entity
-    ics::index::EntityIndex::EntityId entity2Id = indexStore.entity.addEntityId();
+    ics::index::EntityIndex::EntityId entity2Id =
+        indexStore.entity.addEntityId();
     TestComponent comp2{90, 10};
 
-    static bento::protos::AttributeRef createAttrRef(ics::index::EntityIndex::EntityId entityId, const char* attrName) {
+    static bento::protos::AttributeRef createAttrRef(
+        ics::index::EntityIndex::EntityId entityId, const char* attrName) {
         auto attrRef = bento::protos::AttributeRef();
         attrRef.set_component(TEST_COMPONENT_NAME);
         attrRef.set_entity_id(entityId);
@@ -81,7 +85,8 @@ TEST_F(StoresFixture, GetAndSetAttributeRef) {
     auto attrRef = createAttrRef(entity1Id, "height");
     auto& cppAttrRef = getAttributeRef(compStore, indexStore, attrRef);
 
-    ASSERT_EQ(cppAttrRef.primitive().int_64(), comp1.getValue("height").primitive().int_64());
+    ASSERT_EQ(cppAttrRef.primitive().int_64(),
+              comp1.getValue("height").primitive().int_64());
 
     // Modify through cppAttrRef
     cppAttrRef.mutable_primitive()->set_int_64(10);
@@ -94,11 +99,44 @@ TEST_F(StoresFixture, RetrieveNode) {
     auto attrRef = createAttrRef(entity1Id, "width");
 
     auto retrieveNode = bento::protos::Node();
-    auto retrieveAttr = retrieveNode.mutable_retrieve_op()->mutable_retrieve_attr();
+    auto retrieveAttr =
+        retrieveNode.mutable_retrieve_op()->mutable_retrieve_attr();
     retrieveAttr->set_component(TEST_COMPONENT_NAME);
     retrieveAttr->set_entity_id(entity1Id);
     retrieveAttr->set_attribute("width");
 
     auto value = evaluateNode(compStore, indexStore, retrieveNode);
-    ASSERT_EQ(value.primitive().int_64(), comp1.getValue("width").primitive().int_64());
+    ASSERT_EQ(value.primitive().int_64(),
+              comp1.getValue("width").primitive().int_64());
+}
+
+TEST_F(StoresFixture, SwitchNode) {
+    auto node = bento::protos::Node();
+    auto switchOpNode = node.mutable_switch_op();
+    // Set the true and false nodes
+    int trueVal = 10;
+    int falseVal = 30;
+    switchOpNode->mutable_true_node()
+        ->mutable_const_op()
+        ->mutable_held_value()
+        ->mutable_primitive()
+        ->set_int_64(trueVal);
+    switchOpNode->mutable_false_node()
+        ->mutable_const_op()
+        ->mutable_held_value()
+        ->mutable_primitive()
+        ->set_int_64(falseVal);
+
+    // Set the condition node value
+    auto conditionNode = switchOpNode->mutable_condition_node();
+    auto condVal = conditionNode->mutable_const_op()->mutable_held_value();
+    // Set the condition as boolean with true
+    condVal->mutable_primitive()->set_boolean(true);
+    condVal->mutable_data_type()->set_primitive(bento::protos::Type_Primitive_BOOL);
+
+    ASSERT_EQ(evaluateNode(compStore, indexStore, node).primitive().int_64(), trueVal);
+
+    // Set the condition to false
+    condVal->mutable_primitive()->set_boolean(false);
+    ASSERT_EQ(evaluateNode(compStore, indexStore, node).primitive().int_64(), falseVal);
 }
