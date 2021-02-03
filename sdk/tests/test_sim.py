fc2ee247fc8f052b71ff014fc7da4613571f6eac
Zhu Zhanyan <program.nom@gmail.com>
2021-02-03 20:15:53 +0800

    feat(sim): Add Simulation.system to allow users to add systems with @sim.system syntax

--- sdk/tests/test_sim.py
+++ sdk/tests/test_sim.py
@@ -10,29 +10,16 @@ from unittest.mock import Mock
 from bento import types
 from bento.client import Client
 from bento.sim import Simulation
+from bento.graph.plotter import Plotter
 from bento.ecs.spec import EntityDef, ComponentDef, SystemDef
-from bento.protos.sim_pb2 import SimulationDef
 from tests.utils import assert_proto
+from tests.specs import Position, Speed
+from bento.protos.sim_pb2 import SimulationDef
 
 
 @pytest.fixture
 def component_defs():
-    return [
-        ComponentDef(
-            name="position",
-            schema={
-                "x": types.int32,
-                "y": types.int32,
-            },
-        ),
-        ComponentDef(
-            name="health",
-            schema={
-                "current": types.int32,
-                "max": types.int32,
-            },
-        ),
-    ]
+    return [Position, Speed]
 
 
 @pytest.fixture
@@ -67,11 +54,40 @@ def sim(mock_client, component_defs, entity_defs):
     )
 
 
-def test_sim_simulation_start(sim, mock_client, sim_proto):
-    sim.start()
+def test_sim_simulation_init(mock_client, component_defs, entity_defs, sim_proto):
+    # remove ids from entity_def to simulate entity defs created by use without entity_id set.
+    noid_entity_defs = []
+    for entity_def in entity_defs:
+        entity_def.proto.id = 0
+        noid_entity_defs.append(entity_def)
+
+    noid_sim_proto = SimulationDef()
+    noid_sim_proto.CopyFrom(sim_proto)
+    del noid_sim_proto.entities[:]
+    noid_sim_proto.entities.extend([e.proto for e in noid_entity_defs])
+
+    sim = Simulation(
+        name="test_sim",
+        components=component_defs,
+        entities=noid_entity_defs,
+        client=mock_client,
+    )
+
+    # check for apply to obtain entity ids
     mock_client.apply_sim.assert_called_once()
     actual_sim_proto = mock_client.apply_sim.call_args[0][0]
-    assert_proto(actual_sim_proto, sim_proto)
+    assert_proto(actual_sim_proto, noid_sim_proto)
+
+    # check entity ids provided by the engine are applied
+    assert sim.entities[0].id == 1
+
+
+def test_sim_simulation_start(sim, mock_client, sim_proto):
+    sim.start()
+    assert sim.started
+    # test calling again does nothing
+    sim.start()
+    assert sim.started
 
 
 def test_sim_simulation_end(sim, mock_client):
@@ -93,5 +109,20 @@ def test_sim_simulation_with(sim):
 
 def test_sim_simulation_entity(sim, entity_defs, component_defs):
     sim.start()
-    entity = sim.entity([c.name for c in component_defs])
+    component_names = [c.name for c in component_defs]
+    entity = sim.entity(component_names)
     assert entity.entity_id == 1
+    assert frozenset([c.component_name for c in entity.components]) == frozenset(
+        component_names
+    )
+
+
+def test_sim_simulation_system(sim, entity_defs, component_defs):
+    @sim.system
+    def test_sim_fn(g: Plotter):
+        # test that we can use registered entiies and components when plotting
+        car = g.entity(components=[Position, Speed])
+        car[Position].x = 2 * car[Speed].x_neg
+
+    # check system is added
+    assert len(sim.system_defs) == 1
