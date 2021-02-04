a14ca2fd85242c0934c5e6ddf359bcb27a42c9d8
Joel Tio <joeltiojinhon@gmail.com>
2021-02-04 18:24:48 +0800

    feat: update sim struct to create entity Ids and components Add lock to sim struct

--- sim/include/simulation.h
+++ sim/include/simulation.h
@@ -4,6 +4,8 @@
 #include <bento/protos/sim.pb.h>
 #include <core/ics/componentStore.h>
 #include <index/indexStore.h>
+#include <forward_list>
+#include <ics.h>
 
 #include <utility>
 
@@ -14,19 +16,55 @@ struct Simulation {
     bento::protos::SimulationDef simDef;
     ics::ComponentStore compStore;
     ics::index::IndexStore indexStore;
+    // The simulation will be locked when it has started
+    // No changes should be made to the simDef when it is locked
+    bool locked = false;
 
     explicit Simulation(bento::protos::SimulationDef simDef)
         : simDef(std::move(simDef)) {
         // BE CAREFUL: Once simDef is moved into this->simDef, all references to
         // simDef are invalid. Always use this->simDef
+
+        // Create a map from component name to component def for use later
+        std::unordered_map<std::string, bento::protos::ComponentDef> compDefMap;
+        for (size_t i = 0; i < this->simDef.components_size(); i++) {
+            compDefMap[this->simDef.components(i).name()] =
+                this->simDef.components(i);
+        }
+
+        // Retrieve all the existing entity IDs to configure the indexStore
+        std::forward_list<ics::index::EntityIndex::EntityId> configuredIds;
         for (size_t i = 0; i < this->simDef.entities_size(); i++) {
-            if (this->simDef.entities(i).id() == UNSET_ENTITY_ID) {
+            auto& entity = this->simDef.entities(i);
+            if (entity.id() != UNSET_ENTITY_ID) {
+                configuredIds.push_front(entity.id());
+            }
+        }
+
+        // Configure the indexStore with the existing entity IDs
+        indexStore.entity.setEntityIds(configuredIds);
+
+        // Create the components and entity IDs (if needed) for all entities
+        for (size_t i = 0; i < this->simDef.entities_size(); i++) {
+            auto& entity = this->simDef.entities(i);
+            // If the entity ID is unset, create one and set it
+            if (entity.id() == UNSET_ENTITY_ID) {
                 ::google::protobuf::uint32 entityId;
                 do {
                     entityId = indexStore.entity.addEntityId();
                 } while (entityId == UNSET_ENTITY_ID);
 
                 this->simDef.mutable_entities(i)->set_id(entityId);
+            }
+
+            // Create the entity's components
+            for (size_t j = 0; j < entity.components_size(); j++) {
+                const auto& compName = entity.components(j);
+                auto comp = ics::component::UserComponent(compName,
+                                                          compDefMap[compName]);
+                auto compStoreId =
+                    ics::addComponent(indexStore, compStore, comp);
+                indexStore.entity.addComponent(entity.id(), compStoreId);
             }
         }
     }
