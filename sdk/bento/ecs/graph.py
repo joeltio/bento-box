e2a905d74718b441c0610425077b2045a60b346c
Zhu Zhanyan <program.nom@gmail.com>
2021-02-09 19:04:24 +0800

    refactor(sdk): Refactor GraphEntity, GraphComponent to record operations on inputs/outputs dict passed via use_input_outputs()

--- sdk/bento/ecs/graph.py
+++ sdk/bento/ecs/graph.py
@@ -6,18 +6,15 @@
 
 from copy import deepcopy
 from binascii import crc32
-from typing import Any, Iterable, Set, List, Dict
+from typing import Any, Iterable, List, Dict, Union, FrozenSet
 from bento.types import Type
+from bento.utils import to_str_attr
 from bento.ecs.base import Component, Entity
 from bento.ecs.spec import ComponentDef, EntityDef
 from bento.graph.value import wrap_const
 from bento.protos.graph_pb2 import Node
 from bento.protos.references_pb2 import AttributeRef
 from bento.protos.values_pb2 import Value
-
-
-def to_str_attr(attr: AttributeRef):
-    return f"{attr.entity_id}/{attr.component}/{attr.attribute}"
 
 
 class GraphNode:
@@ -46,8 +43,10 @@ class GraphNode:
             )
         )
 
+    # use string as return type for compatiblity with python 3.6
+    # https://stackoverflow.com/a/33533514
     @classmethod
-    def wrap(cls, val: Any):
+    def wrap(cls, val: Any) -> "GraphNode":
         """Wraps the given value val as a Graph, performing type conversions if necessary.
         Args:
             val: Native Value, Node proto to wrap as GraphNode
@@ -98,10 +97,6 @@ class GraphNode:
     def __truediv__(self, other: Any):
         other = type(self).wrap(other)
         return type(self).wrap(Node(div_op=Node.Div(x=self.node, y=other.node)))
-
-    def __truediv__(self, other: Any):
-        other = type(self).wrap(other)
-        return type(self).wrap(Node(div_op=Node.Div(x=other.node, y=self.node)))
 
     def __itruediv__(self, other: Any):
         return self.__truediv__(other)
@@ -160,7 +155,7 @@ class GraphNode:
         )
 
     def __str__(self):
-        return f"{type(self).__name___}<{self.node}>"
+        return f"{type(self).__name__}<{self.node}>"
 
     def __hash__(self):
         return hash(self.node)
@@ -170,7 +165,7 @@ class GraphComponent(Component):
     """Shim that represents an ECS Component when plotting computation graph.
 
     Provides access to component's attributes during graph plotting.
-    Records operations on attributes which can be accessed via `.inputs` and `.outputs` respectively.
+    Records operations on attributes which inputs and outputs dictionaries.
 
         Example:
         # Record operations on component
@@ -188,7 +183,6 @@ class GraphComponent(Component):
         self.__dict__["_name"] = name
         self.__dict__["_schema"] = schema
         # _inputs/_outputs are dict with AttributeRef as key and Retrieve/Mutate nodes as value
-        # ensuring that we record unique input and output nodes
         self.__dict__["_inputs"] = {}
         self.__dict__["_outputs"] = {}
 
@@ -204,11 +198,24 @@ class GraphComponent(Component):
             entity_id=entity_id, name=component_def.name, schema=component_def.schema
         )
 
-    def get_attr(self, name: str) -> Node:
+    def use_input_outputs(
+        self, inputs: Dict[str, GraphNode], outputs: Dict[str, GraphNode]
+    ):
+        """Use the given inputs and output dicts to record attribute operations.
+        Useful for implementing a shared inputs and outputs between multiple GraphComponents.
+
+        Args:
+            inputs:  Dict to record on when attributes are retrieved with get_attr().
+            outputs:  Dict to record on when attributes are set with set_attr().
+        """
+        self.__dict__["_inputs"] = inputs
+        self.__dict__["_outputs"] = outputs
+
+    def get_attr(self, name: str) -> GraphNode:
         attr_ref = AttributeRef(
             entity_id=self._entity_id,
             component=self._name,
-            attribute=name,
+            attribute=str(name),
         )
         # check if attribute has been defined in earlier set_attr()
         # if so return that definition to preserve the graph already built for that attribute
@@ -245,16 +252,6 @@ class GraphComponent(Component):
             )
         )
         self._outputs[to_str_attr(attr_ref)] = set_op
-
-    @property
-    def inputs(self) -> List[GraphNode]:
-        """Get the graph unique input nodes recorded by this Graph component"""
-        return list(self._inputs.values())
-
-    @property
-    def outputs(self) -> List[GraphNode]:
-        """Get the graph unique output nodes recorded by this Graph component"""
-        return list(self._outputs.values())
 
     @property
     def component_name(self):
@@ -298,6 +295,20 @@ class GraphEntity(Entity):
             entity_id=entity_def.id,
         )
 
+    def use_input_outputs(
+        self, inputs: Dict[str, GraphNode], outputs: Dict[str, GraphNode]
+    ):
+        """Use the given inputs and output dicts to record attribute operations.
+        Useful for implementing a shared inputs and outputs between multiple GraphEntities.
+
+        Args:
+            inputs: Dict to record on when this Entity's GraphComponent attributes are retrieved.
+            outputs: Dict to record on when this Entity's GraphComponent attributes are set.
+        """
+        # configure conmponents to use given inputs/outputs dicts
+        for component in self.component_map.values():
+            component.use_input_outputs(inputs, outputs)
+
     def get_component(self, name: str) -> GraphComponent:
         # allow users to specify component class as name
         name = str(name)
@@ -310,8 +321,10 @@ class GraphEntity(Entity):
 
     @property
     def id(self):
+        """Get the id held by this GraphEntity"""
         return self.entity_id
 
     @property
-    def components(self) -> Set[GraphComponent]:
+    def components(self) -> FrozenSet[GraphComponent]:
+        """Get the GraphComponent attached to this GraphEntity"""
         return frozenset(self.component_map.values())
--- sdk/tests/ecs/test_graph.py
+++ sdk/tests/ecs/test_graph.py
@@ -5,12 +5,19 @@
 #
 
 from typing import Set
-from bento.utils import to_yaml_proto
+
+from bento.ecs.graph import (
+    GraphComponent,
+    GraphEntity,
+    GraphNode,
+    to_str_attr,
+    wrap_const,
+)
 from bento.ecs.spec import ComponentDef, EntityDef
-from bento.ecs.graph import GraphEntity, GraphComponent, GraphNode, wrap_const
+from bento.example.specs import Position, Speed
 from bento.protos.graph_pb2 import Node
 from bento.protos.references_pb2 import AttributeRef
-from bento.example.specs import Position
+from bento.utils import to_str_attr, to_yaml_proto
 
 
 def test_graph_ecs_entity():
@@ -42,11 +49,47 @@ def test_graph_ecs_entity_from_def():
     assert has_error
 
 
+def test_graph_ecs_entity_update_input_outputs():
+    # test use_input_outputs() propagates input and output dict to components
+    entity_id, inputs, outputs = 1, {}, {}
+    car = GraphEntity.from_def(
+        entity_def=EntityDef(components=[Position, Speed], entity_id=1),
+        component_defs=[Position, Speed],
+    )
+    car.use_input_outputs(inputs, outputs)
+
+    # get/set should propagate retrieve mutate to inputs and output
+    car_pos_x = car[Position].x
+    car[Position].x = 1
+    car_speed_x = car[Speed].x
+    car[Position].x = 2
+
+    pos_attr_ref = AttributeRef(
+        entity_id=entity_id, component=Position.name, attribute="x"
+    )
+    speed_attr_ref = AttributeRef(
+        entity_id=entity_id, component=Position.name, attribute="x"
+    )
+
+    pos_expected_input = Node(retrieve_op=Node.Retrieve(retrieve_attr=pos_attr_ref))
+    pos_expected_output = Node(
+        mutate_op=Node.Mutate(mutate_attr=pos_attr_ref, to_node=wrap_const(1))
+    )
+    assert inputs[to_str_attr(pos_attr_ref)] == pos_expected_input
+    assert outputs[to_str_attr(pos_attr_ref)] == pos_expected_input
+
+    speed_expected_input = Node(retrieve_op=Node.Retrieve(retrieve_attr=speed_attr_ref))
+    speed_expected_output = Node(
+        mutate_op=Node.Mutate(mutate_attr=speed_attr_ref, to_node=wrap_const(2))
+    )
+    assert inputs[to_str_attr(speed_attr_ref)] == speed_expected_input
+    assert outputs[to_str_attr(speed_attr_ref)] == speed_expected_input
+
+
 def test_graph_ecs_component_from_def():
     entity_id = 1
     position = GraphComponent.from_def(entity_id, Position)
     assert position.component_name == Position.name
-
     # test that we cannnot create from ComponentDef with unset name
     has_error = False
     try:
@@ -57,93 +100,103 @@ def test_graph_ecs_component_from_def():
 
 
 def test_graph_ecs_component_get_attr():
-    entity_id = 1
+    entity_id, inputs, outputs = 1, {}, {}
     position = GraphComponent.from_def(entity_id, Position)
+    position.use_input_outputs(inputs, outputs)
+
     # check that getting an attribute from a component returns a GraphNode
     # wrapping a Retrieve node that retrieves the attribute
     pos_x = position.x
-    expected_node = Node(
-        retrieve_op=Node.Retrieve(
-            retrieve_attr=AttributeRef(
-                entity_id=entity_id, component=Position.name, attribute="x"
-            )
-        )
-    )
+
+    attr_ref = AttributeRef(entity_id=entity_id, component=Position.name, attribute="x")
+    expected_node = Node(retrieve_op=Node.Retrieve(retrieve_attr=attr_ref))
     assert pos_x.node == expected_node
-    # check that component records the retrieve in `.inputs`
-    assert position.inputs[0].node == expected_node
+    # check that component records the retrieve in
+    assert inputs[to_str_attr(attr_ref)].node == expected_node
     # check that retrieving the same attribute only records it once
     pos_y = position.x
-    assert len(position.inputs) == 1
+    assert len(inputs) == 1
 
 
 def test_graph_ecs_component_get_attr_preserve_set_graph():
     entity_id = 1
     position = GraphComponent.from_def(entity_id, Position)
+
     # check that getting an attribute from a component preserves
     # any graph that has already being built by set_attr()
     position.x = 2
+
     pos_x = position.x
-    expected_node = wrap_const(2)
-    assert to_yaml_proto(pos_x.node) == to_yaml_proto(expected_node)
+    expected_node = GraphNode.wrap(2)
+    assert to_yaml_proto(pos_x.node) == to_yaml_proto(expected_node.node)  # type: ignore
 
 
 def test_graph_ecs_component_set_attr_node():
-    entity_id = 1
+    entity_id, inputs, outputs = 1, {}, {}
     position = GraphComponent.from_def(entity_id, Position)
+    position.use_input_outputs(inputs, outputs)
+
     pos_x = position.x
     position.y = 10
     # check setting attribute to node sets expected output node.
     position.y = pos_x
+
+    attr_ref = AttributeRef(
+        entity_id=entity_id,
+        component=Position.name,
+        attribute="y",
+    )
     expected_node = Node(
         mutate_op=Node.Mutate(
-            mutate_attr=AttributeRef(
-                entity_id=entity_id,
-                component=Position.name,
-                attribute="y",
-            ),
+            mutate_attr=attr_ref,
             to_node=pos_x.node,
         )
     )
-    assert position.outputs[0].node == expected_node
+    assert outputs[to_str_attr(attr_ref)].node == expected_node
     # check that setting attribute only takes the last definition
     # the first definition should be ignored since the attribute is redefined
-    assert len(position.outputs) == 1
+    assert len(outputs) == 1
 
 
 def test_graph_ecs_component_set_attr_native_value():
-    entity_id = 1
+    entity_id, inputs, outputs = 1, {}, {}
     position = GraphComponent.from_def(entity_id, Position)
+    position.use_input_outputs(inputs, outputs)
+
     # check setting attribute to native sets expected output node node.
     position.y = 3
+
+    attr_ref = AttributeRef(
+        entity_id=entity_id,
+        component=Position.name,
+        attribute="y",
+    )
     expected_node = Node(
         mutate_op=Node.Mutate(
-            mutate_attr=AttributeRef(
-                entity_id=entity_id,
-                component=Position.name,
-                attribute="y",
-            ),
+            mutate_attr=attr_ref,
             to_node=wrap_const(3),
         )
     )
-    assert position.outputs[0].node == expected_node
+    assert outputs[to_str_attr(attr_ref)] == expected_node
 
 
 def test_graph_ecs_component_set_attr_ignore_self_assign():
-    entity_id = 1
+    entity_id, inputs, outputs = 1, {}, {}
     position = GraphComponent.from_def(entity_id, Position)
+    position.use_input_outputs(inputs, outputs)
 
-    # test assignment should be ignore as we are setting to the attribute to itself
-    position.x = position.x
-
-    assert len(position.outputs) == 0
+    # test assignment should be ignore as we are setting to the attribute to itself position.x = position.x
+    assert len(outputs) == 0
 
 
 def test_graph_ecs_component_aug_assign_node():
-    entity_id = 1
+    entity_id, inputs, outputs = 1, {}, {}
     position = GraphComponent.from_def(entity_id, Position)
+    position.use_input_outputs(inputs, outputs)
+
     # check augment assignment flags the attribute (position.x) as both input and output
     position.y += 30
+
     attr_ref = AttributeRef(
         entity_id=entity_id,
         component=Position.name,
@@ -161,10 +214,10 @@ def test_graph_ecs_component_aug_assign_node():
             ),
         )
     )
-    assert len(position.inputs) == 1
-    assert position.inputs[0].node == expected_input
-    assert len(position.outputs) == 1
-    assert position.outputs[0].node == expected_output
+    assert len(inputs) == 1
+    assert inputs[to_str_attr(attr_ref)] == expected_input
+    assert len(outputs) == 1
+    assert outputs[to_str_attr(attr_ref)] == expected_output
 
 
 def test_graph_ecs_node_wrap():
