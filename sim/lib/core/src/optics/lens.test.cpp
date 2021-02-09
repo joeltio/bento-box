0d852724890b4ff78fe9e5898ca6075b2b0b2030
Joel Tio <joeltiojinhon@gmail.com>
2021-02-09 15:54:46 +0800

    feat: add lens with tests

--- sim/lib/core/src/optics/lens.test.cpp
+++ sim/lib/core/src/optics/lens.test.cpp
@@ -8,15 +8,19 @@ using namespace optics;
 TEST(TEST_SUITE, GetterFromLambda) {
     // From rvalue
     {
-        auto getter = Getter<int, int, char>([](int j, int k) { return 'a'; });
-        ASSERT_EQ(getter(5, 7), 'a');
+        auto getter = Getter<char, int, int>([](int j, int k) { return 'a'; });
+        ASSERT_EQ(getter.get(5, 7), 'a');
     }
 
     // From lvalue
     {
-        auto lambdaGetter = [](int j, int k) { return j + k; };
+        auto lambdaGetter = []<class T, class U>(T&& j, U&& k) {
+            return j + k;
+        };
         auto getter = Getter<int, int, int>(lambdaGetter);
-        ASSERT_EQ(getter(9, 10), 19);
+        // This is to test that get can accept an lvalue
+        auto val = 9;
+        ASSERT_EQ(getter.get(val, 10), 19);
     }
 }
 
@@ -24,7 +28,7 @@ TEST(TEST_SUITE, ComposeGetters) {
     auto getter = Getter<int, int, int>([](int j, int k) { return j + k; });
     auto composedGetter = getter.compose<int>([](int h) { return h + 20; });
 
-    ASSERT_EQ(composedGetter(1, 2), 23);
+    ASSERT_EQ(composedGetter.get(1, 2), 23);
 }
 
 TEST(TEST_SUITE, PrecomposeGetters) {
@@ -32,5 +36,82 @@ TEST(TEST_SUITE, PrecomposeGetters) {
     auto precomposedGetter =
         getter.precompose<int>([](int h) -> int { return h + 20; });
 
-    ASSERT_EQ(precomposedGetter(1), 210);
+    ASSERT_EQ(precomposedGetter.get(1), 210);
+}
+
+TEST(TEST_SUITE, LensFromLambda) {
+    // From rvalue
+    {
+        int storedVal = 3;
+        auto lens =
+            Lens<int, int>([](int&& j) -> int& { return j; },
+                           [](int& j, int newVal) -> void { j = newVal - 10; });
+        ASSERT_EQ(lens.get(3), 3);
+        lens.set(14, storedVal);
+        ASSERT_EQ(storedVal, 4);
+    }
+
+    // From lvalue
+    {
+        int storedVal = 3;
+        auto getter = [](int&& j, int&& k) -> int& { return j; };
+        auto setter = [](int& j, int newVal) { j = newVal - 10; };
+        auto lens = Lens<int, int, int>(getter, setter);
+        // This is to test that get can accept an lvalue
+        ASSERT_EQ(lens.get(storedVal, 10), 3);
+        lens.set(14, storedVal, 5);
+        ASSERT_EQ(storedVal, 4);
+    }
+}
+
+struct B {
+    int height = 0;
+};
+
+struct A {
+    B b;
+};
+
+TEST(TEST_SUITE, ComposeLens) {
+    auto storedVal = A{B{10}};
+    auto getter = [](A&& a) -> B& { return a.b; };
+    auto setter = [](B& b, B&& newB) { b = newB; };
+    auto lens = Lens<B, A>(getter, setter);
+
+    ASSERT_EQ(lens.get(storedVal).height, 10);
+
+    auto getterComp = [](B&& b) -> int& { return b.height; };
+    auto setterComp = [](int& height, int&& newHeight) { height = newHeight; };
+    auto lensComp = Lens<int, B>(getterComp, setterComp);
+
+    auto composedLens = lens.compose(lensComp);
+    ASSERT_EQ(composedLens.get(storedVal), 10);
+    composedLens.set(20, storedVal);
+    ASSERT_EQ(composedLens.get(storedVal), 20);
+}
+
+TEST(TEST_SUITE, CompositionCopiesLenses) {
+    // Create the lenses
+    auto getter = [](A&& a) -> B& { return a.b; };
+    auto setter = [](B& b, B&& newB) { b = newB; };
+    auto lensAPtr = new Lens<B, A>(getter, setter);
+
+    auto getterComp = [](B&& b) -> int& { return b.height; };
+    auto setterComp = [](int& height, int&& newHeight) { height = newHeight; };
+    auto lensBPtr = new Lens<int, B>(getterComp, setterComp);
+
+    auto composedLens = lensAPtr->compose(*lensBPtr);
+
+    // Delete the lenses
+    delete lensAPtr;
+    lensAPtr = nullptr;
+    delete lensBPtr;
+    lensBPtr = nullptr;
+
+    // Use the composed lens
+    auto storedVal = A{B{10}};
+    ASSERT_EQ(composedLens.get(storedVal), 10);
+    auto j = 20;
+    composedLens.set(j, storedVal);
+    ASSERT_EQ(composedLens.get(storedVal), 20);
 }
