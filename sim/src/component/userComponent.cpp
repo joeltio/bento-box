c547d44b143776a1f033475e3805224d0514cb12
Joel Tio <joeltiojinhon@gmail.com>
2021-02-10 15:30:21 +0800

    feat: update user component to throw errors for incorrect data types

--- sim/src/component/userComponent.cpp
+++ sim/src/component/userComponent.cpp
@@ -1,4 +1,5 @@
 #include <component/userComponent.h>
+#include <google/protobuf/util/message_differencer.h>
 
 namespace ics::component {
 
@@ -16,6 +17,20 @@ void UserComponent::setValue(const std::string& attrName,
                              const bento::protos::Value& value) {
     if (!compDef.schema().contains(attrName)) {
         throw std::out_of_range("No such attribute name: " + attrName);
+    }
+
+    if (!value.has_data_type()) {
+        throw std::runtime_error(
+            "Missing data type when setting value for attribute " + attrName);
+    }
+
+    auto schemaType = compDef.schema().at(attrName);
+    auto differencer = google::protobuf::util::MessageDifferencer();
+    if (!differencer.Compare(schemaType, value.data_type())) {
+        throw std::runtime_error(
+            "Data type of given value does not match schema type for "
+            "attribute " +
+            attrName);
     }
 
     values[attrName] = value;
