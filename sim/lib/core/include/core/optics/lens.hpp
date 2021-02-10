4713cdb233e1e7d279c589ab764465afd6a2db0a
Joel Tio <joeltiojinhon@gmail.com>
2021-02-10 18:16:23 +0800

    refactor: lint lens.hpp

--- sim/lib/core/include/core/optics/lens.hpp
+++ sim/lib/core/include/core/optics/lens.hpp
@@ -1,53 +1,51 @@
-#ifndef BENTOBOX_LENS_H
-#define BENTOBOX_LENS_H
+#ifndef BENTOBOX_LENS_HPP
+#define BENTOBOX_LENS_HPP
 
 #include <functional>
 #include <type_traits>
 
 namespace {
-    template<class ...Args>
-    struct is_same_args {
-        // This static constexpr function compares the given type arguments and
-        // ensures that they are the same as Args
-        template<size_t N = 0, class ...OtherArgs>
-        static constexpr bool _as() {
-            // constexpr is required here because N is a non-type template
-            // argument
-            if constexpr (N == sizeof...(OtherArgs)) {
-                return true;
-            } else if (std::is_same_v<
-                std::remove_reference_t<
-                    std::tuple_element_t<N, std::tuple<OtherArgs...>>>,
-                std::remove_reference_t<
-                    std::tuple_element_t<N, std::tuple<Args...>>>
-            >) {
-                return _as<N+1, OtherArgs...>();
-            } else {
-                return false;
-            }
+template <class... Args>
+struct is_same_args {
+    // This static constexpr function compares the given type arguments and
+    // ensures that they are the same as Args
+    template <size_t N = 0, class... OtherArgs>
+    static constexpr bool _as() {
+        // constexpr is required here because N is a non-type template
+        // argument
+        if constexpr (N == sizeof...(OtherArgs)) {
+            return true;
+        } else if (std::is_same_v<
+                       std::remove_reference_t<
+                           std::tuple_element_t<N, std::tuple<OtherArgs...>>>,
+                       std::remove_reference_t<
+                           std::tuple_element_t<N, std::tuple<Args...>>>>) {
+            return _as<N + 1, OtherArgs...>();
+        } else {
+            return false;
         }
+    }
 
-        // Alias to the recursive comparison constexpr function
-        template<class ...OtherArgs>
-        static constexpr bool as() {
-            return _as<0, OtherArgs...>();
-        }
-    };
-}
+    // Alias to the recursive comparison constexpr function
+    template <class... OtherArgs>
+    static constexpr bool as() {
+        return _as<0, OtherArgs...>();
+    }
+};
+}  // namespace
 
 namespace optics {
 
-    // Note on std::move vs static_cast<std::remove_reference_t<T>&&>
-    // Later in the code, you will see that I have used static_cast instead of
-    // std::move. The reason is that we need to cast to an rvalue reference
-    // because that's the type of the function's parameter.
-    // std::move is not used because it is semantically incorrect, though it
-    // does the same thing.
+// Note on std::move vs static_cast<std::remove_reference_t<T>&&>
+// Later in the code, you will see that I have used static_cast instead of
+// std::move. The reason is that we need to cast to an rvalue reference
+// because that's the type of the function's parameter.
+// std::move is not used because it is semantically incorrect, though it
+// does the same thing.
 
 template <class Return, class... Args>
 class Getter {
    protected:
-
    public:
     const std::function<Return(std::remove_reference_t<Args>&&...)> getter;
     // A getter cannot be constructed without a getter function
@@ -57,9 +55,9 @@ class Getter {
     explicit Getter(const std::function<Return(Args...)>& getter)
         : getter(getter) {}
 
-    template<class ...RArgs>
-    requires (is_same_args<Args...>::template as<RArgs...>())
-    Return get(RArgs&&... args) const {
+    template <class... RArgs>
+    requires(is_same_args<Args...>::template as<RArgs...>()) Return
+        get(RArgs&&... args) const {
         return getter(static_cast<std::remove_reference_t<RArgs>>(args)...);
     }
 
@@ -90,10 +88,10 @@ class Getter {
         // Require that the type Fn is something invocable with the NewArgs and
         // returns the first argument
         std::is_invocable_r_v<
-            std::tuple_element_t<0, std::tuple<Args...>>,
-            Fn,
-            NewArgs...>
-        Getter<Return, NewArgs...> precompose(Fn&& precomposeGetter) const {
+            std::tuple_element_t<0, std::tuple<Args...>>, Fn,
+            NewArgs...> Getter<Return,
+                               NewArgs...> precompose(Fn&& precomposeGetter)
+            const {
         // Make a copy of the getter so that if the current Getter is destroyed
         // The returned getter is still usable
         std::function<Return(Args...)> getterCopy(getter);
@@ -125,18 +123,18 @@ class Lens {
     Lens(const GetterFn& getter, const SetterFn& setter)
         : getter(getter), setter(setter) {}
 
-    template<class ...RArgs>
-    requires (is_same_args<Args...>::template as<RArgs...>())
-    Return& get(RArgs&&... args) const {
+    template <class... RArgs>
+    requires(is_same_args<Args...>::template as<RArgs...>()) Return& get(
+        RArgs&&... args) const {
         return getter(static_cast<std::remove_reference_t<RArgs>&&>(args)...);
     }
 
-    template<class RReturn, class ...RArgs>
-    // Ensure that the arguments are the correct type
-    requires (is_same_args<Args...>::template as<RArgs...>())
-    // Ensure that RReturn is the correct type
-        && std::is_same_v<std::remove_reference_t<RReturn>, Return>
-    void set(RReturn&& val, RArgs&&... args) const {
+    template <class RReturn, class... RArgs>
+        // Ensure that the arguments are the correct type
+        requires(is_same_args<Args...>::template as<RArgs...>())
+        // Ensure that RReturn is the correct type
+        && std::is_same_v<std::remove_reference_t<RReturn>, Return> void set(
+               RReturn&& val, RArgs&&... args) const {
         auto& ref = get(std::forward<RArgs>(args)...);
         this->setter(ref, static_cast<std::remove_reference_t<RReturn>&&>(val));
     }
@@ -144,19 +142,20 @@ class Lens {
     template <class NewReturn>
     Lens<NewReturn, Args...> compose(
         const Lens<NewReturn, Return>& otherLens) const {
-        std::function<NewReturn&(Args&&...)> get = [
-            getter = this->getter,
-            otherGetter = otherLens.getter
-        ]<class ...PArgs>(PArgs&&... args) -> NewReturn& {
-              return otherGetter(
-                  std::forward<Return>(getter(std::forward<PArgs>(args)...)));
+        std::function<NewReturn&(Args && ...)> get =
+            [
+                getter = this->getter, otherGetter = otherLens.getter
+            ]<class... PArgs>(PArgs && ... args)
+                ->NewReturn& {
+            return otherGetter(
+                std::forward<Return>(getter(std::forward<PArgs>(args)...)));
         };
 
-        std::function<void(NewReturn&, NewReturn&&)> set = otherLens.setter;
+        std::function<void(NewReturn&, NewReturn &&)> set = otherLens.setter;
         return Lens<NewReturn, Args...>(get, set);
     }
 };
 
 }  // namespace optics
 
-#endif  // BENTOBOX_LENS_H
+#endif  // BENTOBOX_LENS_HPP
